a100f2175b8f685dd8c24b2824f4bdc795ba97a5
Mikiyas Mebrate <mikiyasmebrate@gmail.com>
2025-02-19 11:09:53 +0300

    Video model Comment added

--- core/Videos/models.py
+++ core/Videos/models.py
@@ -15,8 +15,9 @@ class Video(models.Model):
         self.save()
     
     def remove_like(self):
-        self.like -= 1
-        self.save()
+        if self.like > 0:
+            self.like -= 1
+            self.save()
     
 class VideoComment(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
@@ -27,6 +28,24 @@ class VideoComment(models.Model):
 
     def __str__(self):
         return self.comment
+
+class VideoCommentLike(models.Model):
+    comment = models.ForeignKey(VideoComment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    like = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)  
+
+    def __str__(self):
+        return self.user.email
+
+    def add_like(self):
+        self.like += 1
+        self.save()
+    
+    def remove_like(self):
+        if self.like > 0:
+            self.like -= 1
+            self.save()
 
 class VideoLike(models.Model):
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
