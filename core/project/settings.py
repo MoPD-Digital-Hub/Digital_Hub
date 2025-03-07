ea63bda5c1b8cece96a9f2146fd6904bee4c20d6
Mikiyas Mebrate <mikiyasmebrate@gmail.com>
2025-03-07 13:30:34 +0300

    setting base dir updated

--- core/project/settings.py
+++ core/project/settings.py
@@ -8,7 +8,7 @@ import logging.handlers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-LOG_DIR = os.path.join(BASE_DIR, 'logs')
+LOG_DIR = os.path.join(BASE_DIR.parent, 'logs')
 
 
 load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
