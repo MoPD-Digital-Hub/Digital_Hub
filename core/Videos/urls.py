c7ab98f31641e3b72fce4c171b9f27e229aacd15
Mikiyas Mebrate <mikiyasmebrate@gmail.com>
2025-03-08 11:44:00 +0300

    comment api updated

--- core/Videos/urls.py
+++ core/Videos/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from Videos.api.api import video_api , comments_api , toggle_like , like_comment_api
+from Videos.api.api import video_api , comments_api , toggle_like , toogle_like_comment
 
 
 urlpatterns = [
     path('', video_api),
     path('comments/<int:video_id>/', comments_api),
     path('toggle-like/<int:video_id>/' ,  toggle_like),
-    path('like_comment/<int:comment_id>/' ,  like_comment_api)
+    path('toogle-like-comment/<int:comment_id>/' ,  toogle_like_comment)
 ]
