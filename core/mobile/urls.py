9f8cfed9d3f0a8f44a879b3d8079bcf287e4ed0f
Mikiyas Mebrate <mikiyasmebrate@gmail.com>
2025-03-11 10:32:33 +0300

    dashboard url updated

--- core/mobile/urls.py
+++ core/mobile/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from mobile.api import api
 
 urlpatterns = [
-    path('dashbord/apps/', api.app),
-    path('dashbord/lastest-videos/', api.latest_videos),
+    path('dashboard/apps/', api.app),
+    path('dashboard/lastest-videos/', api.latest_videos),
 ]
