ad11bdbfce6e2214c037c0287cbf29d3360ea546
Mikiyas Degefu <mikiyasmebrate2656@gmail.com>
2025-04-03 13:17:09 +0300

    email updated

--- core/userManagement/tasks/email_tasks.py
+++ core/userManagement/tasks/email_tasks.py
@@ -1,6 +1,13 @@
+import os
+from pathlib import Path
 from __future__ import absolute_import, unicode_literals
 from django.core.mail import send_mail
 from celery import shared_task
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR.parent, '.env'))
 
 
 @shared_task
@@ -8,7 +15,7 @@ def send_email(email, token):
     send_mail(
         "Subject here",
         f"MoPD Digital Hub OTP: {token}",
-        "mikiyasmebrate2656@gmail.com",
+        os.getenv('EMAIL'),
         [email],
         fail_silently=False,
     )
