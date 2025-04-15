5dc85b850e532cdd3d115910b8b913dc78be687c
KaleabHegie <benjiyg400@gmail.com>
2025-04-15 11:03:01 +0300

    Url Changed

--- core/mobile/urls.py
+++ core/mobile/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from mobile.api import api
+from mobile.api import api 
 
 urlpatterns = [
     path('dashboard/apps/', api.app),
     path('dashboard/lastest-videos/', api.latest_videos),
+    path('setting/' , api.setting),
+    path('faq/' , api.faq),
+    path('contact-us/' , api.contact_us)
+    
 ]
