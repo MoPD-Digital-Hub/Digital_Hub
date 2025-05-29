8032cd5c9e3aea439fc97189eb2d6cd54e8674d3
Mikiyas <mikiyas.m.degefu@gmail.com>
2025-05-29 13:50:51 +0300

    comment user resolved

--- core/userManagement/models.py
+++ core/userManagement/models.py
@@ -6,7 +6,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     photo = models.ImageField(upload_to='User/Photo', null=True, blank=True)
     is_first_time = models.BooleanField(default = True)
-    excellence = models.CharField(max_length=100, blank=True, null=True)
+    excellence = models.CharField(max_length=100, blank=True, null=True, default='Mr')
     bio = models.CharField(max_length=100, null=True, blank=True)
     token = models.CharField(max_length=600, null=True, blank=True)
     tokenExpiration = models.DateTimeField(null=True, blank=True)
