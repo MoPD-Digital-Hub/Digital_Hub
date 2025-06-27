14a78487d8014cba2aa05ad50ce8118940436f99
KaleabHegie <benjiyg400@gmail.com>
2025-06-27 17:19:03 +0300

    Privacy started

--- core/mobile/views.py
+++ core/mobile/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+from . models import *
+
+
+def privacy_policy(request):
+    privacy_policy = Setting.objects.get(id=1)
+    return render(request , 'privacy_policy.html' , privacy_policy)
