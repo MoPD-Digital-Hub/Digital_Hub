8c9e1621197ca3b44b3184eeaf56078328b48c09
mopd-server <server@mopd.com>
2025-10-12 19:12:50 +0000

    fix: AI chat resonse

--- core/AI/admin.py
+++ core/AI/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Document)
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'file', 'is_loaded')  # Columns shown in the admin list view
+    list_filter = ('is_loaded',)  # Sidebar filter
+    search_fields = ('name',)  # Search bar for document names
+    list_editable = ('is_loaded',)  # Allows toggling 'is_loaded' directly in the list view
+    ordering = ('name',)  # Sort alphabetically by default
+
+
 admin.site.register(LoadedFile)
 admin.site.register(ChatInstance)
 admin.site.register(QuestionHistory)
